ad817cb738ae55e49e861fd3113f3429669d6726
Linux <ypopov2005@gmail.com>
2025-01-11 02:49:35 +0300

    writing main.py

--- reviews_wall/main.py
+++ reviews_wall/main.py
@@ -1,4 +1,22 @@
+from aiohttp import web # Аиохттп
+import jinja2 # Шаблонизатор jinja2
+import aiohttp_jinja2
 
+# В этой функции производится настройка путей для всего приложения
+def setup_routes(application):
+    # Импорт
+    from app.forum.routes import setup_routes as setup_from_routes
+    setup_from_routes(application)
+
+def setup_external_libraries(application: web.Application) -> None:
+    # указываем шаблонизатору, что html-шаблоны надо искать в папке templates
+    aiohttp_jinja2.setup(application, loader=jinja2.FileSystemLoader("templates"))
 
 if __name__ == "__main__":
-    print(1)
+    application = web.Application() # Создаем веб-сервер
+
+    # Настройка веб-сервера
+    setup_external_libraries(application)
+    setup_routes(application)
+    # Запускаем веб-сервер
+    web.run_app(application)
